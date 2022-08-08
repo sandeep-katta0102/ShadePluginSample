ced982a3d67245ae4700a130487c0c7f09fd4b33
sandeep katta <sandeep.katta2007@gmail.com>
2022-08-08 16:57:40 +0530

    fix compile issue

--- src/main/scala/com/example/shade/DeltaDependent.scala
+++ src/main/scala/com/example/shade/DeltaDependent.scala
@@ -1,6 +1,5 @@
 package com.example.shade
 
-// import org.apache.spark.sql.delta.DeltaTable
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.delta.DeltaLog
 class DeltaDependent {
@@ -14,17 +13,9 @@ class DeltaDependent {
     // val deltaTableUsingDB = com.databricks.sql.transaction.tahoe.DeltaLog.forTable(spark, path)
 
     val deltaTable = DeltaLog.forTable(spark.asInstanceOf[SparkSession], path)
-    deltaTable.snapshot.version
     deltaTable.doLogCleanup()
+    deltaTable.snapshot.version
 
 
-
-//    %scala
-//    import com.databricks.sql.transaction.tahoe._
-//
-//    val deltaLog = com.databricks.sql.transaction.tahoe.DeltaLog.forTable(spark, deltaTablePath)
-//    val currentFiles = deltaLog.snapshot.allFiles
-//    display(currentFiles.groupBy("partitionValues.col1", "partitionValues.col2")
-//      .count().orderBy($"count".desc))
   }
 }
